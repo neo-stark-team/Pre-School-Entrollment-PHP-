fec14670dff1425a29e950cf1b0eccc3bfe88e5b
Mathankumar R <rmathankumarcpm@gmail.com>
2023-04-13 14:42:47 +0530

    commit on 13/02 - 2pm

--- preschool/includes/config.php
+++ preschool/includes/config.php
@@ -2,7 +2,7 @@
 //time zone
 date_default_timezone_set('Asia/Kolkata');
 //database connection
-$con=mysqli_connect("database-1.c7j0ofgsqkxi.us-east-1.rds.amazonaws.com","admin","","preschooldb");
+$con=mysqli_connect("database-1.c7j0ofgsqkxi.us-east-1.rds.amazonaws.com","admin","examly2023","preschooldb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
